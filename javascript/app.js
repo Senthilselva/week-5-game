a609b4d82e856f3dc27c4485bbc10ca626e8e4d7
Senthilselva <senthilselvak@gmail.com>
2016-08-30 00:11:11 -0400

    just timer

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,60 @@
 $( document ).ready(function() {
 
+//***********************************************************
+//****************Time Functions ****************************
+//**********************************************************
+var startAt=10;
+var time = startAt;
+
+function displayNumber(){
+    console.log(time);
+    $('#clock').html('<h2>' + time + '</h2>');
+}
+
+function run(){
+    counter = setInterval(decrement, 1000);
+}
+
+function reset() {
+    time=startAt;
+    displayNumber();
+}
+
+// The decremeent function.
+function decrement(){
+    // Decrease number by one.
+    time--;
+    // Show the number in the #show-number tag.
+    displayNumber();
+    // Once number hits zero...
+    if (time === 0){
+        // ...run the stop function.
+        stop();
+        showAnsPic();
+        // Alert the user that time is up.
+        //alert('Time Up!');
+    }
+}
+        // The stop function
+function stop(){
+    clearInterval(counter);
+}
+//*************************time functions*****************
+
+
 //Question object
-function questionObj(question,answerLs,posOfRightAnswer){
-  this.question=question;
-  this.answerList = [];
-  for(var i=0; i<4; i++){
+function questionObj(question,answerLs,posOfRightAnswer,pic){
+    this.question=question;
+    this.answerList = [];
+    for(var i=0; i<4; i++){
         if( i == posOfRightAnswer ){
           this.answerList.push({ans:answerLs[i],isAns:true});
         }
         else{
           this.answerList.push({ans:answerLs[i],isAns:false});
         }
-  }
+    }
+    this.picture=pic;
   // console.log(this);
 };
 
@@ -19,9 +62,9 @@ function questionObj(question,answerLs,posOfRightAnswer){
 var anslist=[];
 var questionList=[];
 anslist[1]=["Dom Perignon","Monte Cassino","Cluny Abbey","erheh"];
-questionList.push(new questionObj("Who was the legendary Benedictine monk who invented champagne?",anslist[1],0));
+questionList.push(new questionObj("Who was the legendary Benedictine monk who invented champagne?",anslist[1],0,"images/dom_perignon.gif"));
 anslist[2]=["Lake Erie","Great Salt Lake","Lake St.Clair","Lake Superior"];
-questionList.push(new questionObj("Name the largest freshwater lake in the world?",anslist[2],3));
+questionList.push(new questionObj("Name the largest freshwater lake in the world?",anslist[2],3,"images/lake_superior.gif"));
 // console.log(questionList);
 
 
@@ -36,6 +79,7 @@ selectedQuestion=[];
 wins = 0;
 losses = 0;
 questionNum =0;
+time = startAt;
 //hide the last page
 //make the question visible 
 };
@@ -47,125 +91,45 @@ function displayQusAndAns(){
 
          //display answers on the screen
     for(var j=0; j < selectedQuestion.answerList.length; j++){
-        var ansdiv = "#optionsRadios"+j;
-        $(ansdiv).attr("data-num",selectedQuestion.answerList[j].isAns);
-        $(ansdiv)[0].nextSibling.textContent = selectedQuestion.answerList[j].ans;
-         //$(ansdiv).after(selectedQuestion.answerList[j].ans);
+        var ansbutton = "#optionsRadios"+j;
+        $(ansbutton).attr("data-num",selectedQuestion.answerList[j].isAns);
+        $(ansbutton)[0].nextSibling.textContent = selectedQuestion.answerList[j].ans;
+         //$(ansbutton).after(selectedQuestion.answerList[j].ans);
         }
 }; //closing display function
 
 
-function timeConverter(t){
-   var minutes = Math.floor(t/60);
-   var seconds = t - (minutes * 60);
-   if (seconds < 10){
-     seconds = "0" + seconds;
-   }
-   if (minutes === 0){
-     minutes = "00";
-   } else if (minutes < 10){
-     minutes = "0" + minutes;
-   }
-   return minutes + ":" + seconds;
- } //convert time
+//Select a question not a timer fuction
+function selectAQuestion(){
+    for( ;questionNum<questionList.length;questionNum++){
+        selectedQuestion = questionList[questionNum];
+        console.log(selectedQuestion);
+    $('#answerDiv').show();
+    $('#pictureDiv').hide();
 
-// // Our stopwatch object
-// var stopwatch = {
-//     time:0,
+        return;
+    }
+};
 
-//     reset: function(){
-//         stopwatch.time = 30;
-//         // DONE: Change the "display" div to "00:00."
-//         $('#clock').html('30:00');
-//     },
-//     start: function(){
-//         // DONE: Use setInterval to start the count here.
-//         counter = setInterval(stopwatch.count, 1000);
-//     },
-//     stop: function(){
-//         // DONE: Use clearInterval to stop the count here.
+function showAnsPic(){
+    $('#answerDiv').hide();
+    $('#pictureDiv').show();
+    var pichold=$('#ansPic');
+    console.log(selectedQuestion.picture);
+    pichold.attr('src',selectedQuestion.picture);
+}
 
-//         clearInterval(counter);
-//        // stopwatch.reset();
-//         //alert("inside stop");
-//     },
-//     count: function(){
-//         // DONE: increment time by 1, remember we cant use "this" here.
-//         stopwatch.time--;
-//         // DONE: Get the current time, pass that into the stopwatch.timeConverter function, 
-//         //       and save the result in a variable.
-//         var converted = stopwatch.timeConverter(stopwatch.time);
-//         // DONE: Use the variable you just created to show the converted time in the "display" div.
-//         if(stopwatch.time <= 0){
-//             //alert(stopwatch.time);
-//             stopwatch.stop();
-//         }
-//         $('#clock').html(converted);
-        
-//     },
-//     timeConverter: function(t){
-//         var minutes = Math.floor(t/60);
-//         var seconds = t - (minutes * 60);
-//         if (seconds < 10){
-//             seconds = "0" + seconds;
-//         }
-//         if (minutes === 0){
-//             minutes = "00";
-//         } else if (minutes < 10){
-//             minutes = "0" + minutes;
-//         }
-//         return minutes + ":" + seconds;
-//     }
-// }; // closing the stopwatch object
+newGame();
+selectAQuestion();
+displayQusAndAns();
+run();
 
-// // stopwatch.reset();
-// // stopwatch.start();
 
-// //Question object
-// function questionObj(question,answerLs,posOfRightAnswer){
-// 	this.question=question;
-// 	this.answerList = [];
-// 	for(var i=0; i<4; i++){
-// 		if( i == posOfRightAnswer ){
-// 			this.answerList.push({ans:answerLs[i],isAns:true});
-// 		}
-// 		else{
-// 			this.answerList.push({ans:answerLs[i],isAns:false});
-// 		}
-// 	}
-// 	// console.log(this);
-// };
+}); //closing on Document ready
 
-// var anslist=[];
-// var questionList=[];
-// anslist[1]=["Dom Perignon","Monte Cassino","Cluny Abbey","erheh"];
-// questionList.push(new questionObj("Who was the legendary Benedictine monk who invented champagne?",anslist[1],0));
-// anslist[2]=["Lake Erie","Great Salt Lake","Lake St.Clair","Lake Superior"];
-// questionList.push(new questionObj("Name the largest freshwater lake in the world?",anslist[2],3));
 
-// //console.log(questionList);
-// //Gobal Variable
-// var selectedQuestion;
-// var wins=0;
-// var losses=0;
 
-// function displayQusAndAns(){
-//     //displays the question on the screen
-//     $(".question").empty();
-//     $(".question").html(selectedQuestion.question);
 
-//         //display answers on the screen
-//     for(var j=0; j < selectedQuestion.answerList.length; j++){
-//         var ansdiv = "#optionsRadios"+j;
-//         $(ansdiv).attr("data-num",selectedQuestion.answerList[j].isAns);
-//         $(ansdiv)[0].nextSibling.textContent = selectedQuestion.answerList[j].ans;
-//         //$(ansdiv).after(selectedQuestion.answerList[j].ans);
-//         }
-// }//closing display function
-//     stopwatch.reset();
-
-// stopwatch.start();
-// function startGame(){
 	
 // 	//make the lastPage hidden
 // 	//to be done 
