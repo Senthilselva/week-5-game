5fa15fae2215135fe97e6b6e28cb0b8a3dd3ffa6
Senthilselva <senthilselvak@gmail.com>
2016-08-31 16:26:05 -0400

    added a quick fix because data-num was not working

--- javascript/app.js
+++ javascript/app.js
@@ -38,14 +38,16 @@ function questionObj(question,answerLs,posOfRightAnswer,pic){
     this.answerList = [];
     for(var i=0; i<4; i++){
         if( i == posOfRightAnswer ){
-          this.answerList.push({ans:answerLs[i],isAns:true});
+          this.answerList.push({ans:answerLs[i],isAns:1});
         }
         else{
-          this.answerList.push({ans:answerLs[i],isAns:false});
+          this.answerList.push({ans:answerLs[i],isAns:0});
         }
     }
     this.picture=pic;
-  // console.log(this);
+    //this is added as a quick fix instead of going through the data-num route
+    this.rightAnswer = answerLs[posOfRightAnswer];
+  console.log(this);
 };
 
 
@@ -85,35 +87,36 @@ function newGame(){
 function stop(){
     feedback = "Time is Up";
     showAnsPic();
-}
+};
 
 
 function displayQusAndAns(){
     //displays the question on the screen
-    
     clearTimeout(picWindowTimeOut);
-
     $(".question").empty();
     $(".question").html(selectedQuestion.question);
-
          //display answers on the screen
     for(var j=0; j < selectedQuestion.answerList.length; j++){
         var ansbutton = "#optionsRadios"+j;
-        $(ansbutton).attr("data-num",selectedQuestion.answerList[j].isAns);
+        $('ansbutton').attr('data-num');
+        console.log("before assignment" + $(ansbutton).data('num'));
+        $(ansbutton).data('num',selectedQuestion.answerList[j].isAns);
         $(ansbutton)[0].nextSibling.textContent = selectedQuestion.answerList[j].ans;
+        console.log($(ansbutton)[0].nextSibling.textContent+" "+selectedQuestion.answerList[j].isAns+"  "+ $(ansbutton).data('num'));
          //$(ansbutton).after(selectedQuestion.answerList[j].ans);
-        console.log()
     }
-    
-    return;  
+    //return;  
 }; //closing display function
 
 
 //Select a next question fuction
 function selectAQuestion(){
     if(questionNum<questionList.length){
+    //$('input').removeData('num');
+
         selectedQuestion = questionList[questionNum];
-       // console.log(selectedQuestion);
+    
+    
     $('#answerDiv').show();
     $('#pictureDiv').hide();
         questionNum++
@@ -128,10 +131,17 @@ function showAnsPic(){
     //When showing the picture Answer time out is cancelled
     clearTimeout(ansWindowTimeOut);
 
- 
-    //get the text behind the right answer using data-num    
-    var rightAnsId = '#' + $('input[data-num = "true"]').attr('id');
-    var rightAnswer = $(rightAnsId)[0].nextSibling.textContent;
+//******************************************************************************
+    //get the text behind the right answer using data-num   
+   // $('input[data-num = "true"]'). 
+    // var rightAnsId = '#' + $('input[data-num = 1]').attr('id');
+    // console.log("rightAnsId"+ rightAnsId);
+    // var rightAnswer = $(rightAnsId)[0].nextSibling.textContent;
+//there is a bug need to be fixed later ********************************************
+
+//************************8quick fix**************************************8
+
+var rightAnswer =  selectedQuestion.rightAnswer;
 
     feedback = feedback + "<br>" + "The right Answer is " + rightAnswer;
     //display the right answer in questin div
@@ -147,7 +157,7 @@ function showAnsPic(){
     picWindowTimeOut = setTimeout(playgame, 3000);
 //console.log("value of picWindowTimeOut " + picWindowTimeOut);    
 
-}
+};
 
 function playgame(){
 selectAQuestion();
@@ -166,25 +176,24 @@ run();
 
 //if the radio button is clicked 
     $('input').on('click',function(){
+        debugger;
+
         //check to see if data num value which is set to true or false depending on the txt behind it
-        
         console.log(this);
-        console.log($(this)[0].nextSibling.textContent);
-        console.log($(this).data('num'));
+        console.log("nextsibiling" + $(this)[0].nextSibling.textContent);
+        console.log("data num" + $(this).data('num'));
         var check = $(this).data('num');
 
-        debugger;
         
         if(check == true) {
             //feedback = $('input[data-num = "true"]').val();
            feedback = "Awesome! You are right!";
-
         } else {
             feedback = "Nope! You are wrong";
             //alert("wrong answer");
         } 
+        clearTimeout(picWindowTimeOut); 
         $(this).attr('checked', false);
-        clearTimeout(picWindowTimeOut);  
         showAnsPic();
     });//Clicking on the radio button
 
